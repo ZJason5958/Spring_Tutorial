5b213a469f6f705d230fa14e45b05b069208c31a
ZJason5958 <zjason5958@gmail.com>
2025-04-18 00:27:35 -0400

    Done testing @Autowired.

--- store/src/main/java/com/speedrunspring/store/OrderService.java
+++ store/src/main/java/com/speedrunspring/store/OrderService.java
@@ -1,10 +1,15 @@
 package com.speedrunspring.store;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class OrderService {
     private PaymentService paymentService;
-    // public OrderService(PaymentService paymentService) {
-    //     this.paymentService = paymentService;
-    // }
+    
+    public OrderService(PaymentService paymentService) {
+        this.paymentService = paymentService;
+    }
     public void setPaymentService(PaymentService paymentService) {
         this.paymentService = paymentService;
     }
